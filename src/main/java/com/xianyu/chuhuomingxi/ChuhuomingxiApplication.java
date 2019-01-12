51bb0d33c353accd18df68d6e0e50511dec7fcd6
bruobuqi <1193921715@qq.com>
2019-01-12 16:56:53 +0800

    创始

---
+++ src/main/java/com/xianyu/chuhuomingxi/ChuhuomingxiApplication.java
@@ -0,0 +1,14 @@
+package com.xianyu.chuhuomingxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuhuomingxiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuhuomingxiApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/xianyu/chuhuomingxi/ChuhuomingxiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xianyu.chuhuomingxi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChuhuomingxiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
